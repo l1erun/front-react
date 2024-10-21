e0c4e3038764509b01755e54a83b2c9b1c7de1a9
Алексей Степанов <aleksejstepanov@MacBook-Air-Aleksej.local>
2024-10-21 23:08:22 +0300

    Приступил к реализации логики игры

--- src/api/map/session.js
+++ src/api/map/session.js
@@ -42,3 +42,28 @@ export const deleteUserInSession = async () => {
         throw error; // Пробрасываем ошибку дальше, чтобы обработать её на уровне UI
     }
 };
+
+export const startGameSession = async (session) => {
+    try {
+        const response = await fetch('http://localhost:8080/sessions/start', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(session.sessionId),
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text(); // Чтение текста ошибки
+            throw new Error(errorMessage || 'Ошибка авторизации');
+        }
+        const data = await response.json(); // Обрабатываем ответ как JSON
+        console.log('Response data:', data); // Логируем ответ
+
+        return data;
+        // return await response.json(); // Если всё прошло успешно, возвращаем JSON с токеном
+    } catch (error) {
+        console.error('Ошибка при авторизации:', error.message);
+        throw error; // Пробрасываем ошибку дальше, чтобы обработать её на уровне UI
+    }
+};
--- src/components/map/GameSession.js
+++ src/components/map/GameSession.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {creatSession} from "../../api/map/session";
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {creatSession, startGameSession} from "../../api/map/session";
 
 function GameSession() {
     const [sessionId, setSessionId] = useState(null);
+    const [session, setSession] = useState(null);
     const navigate = useNavigate();
 
     const startSession = async () => {
         try {
             const session = await creatSession(); // Логика генерации или получения UUID сессии
-            console.log(session)
+            console.log(session);
+            setSession(session);
+            setSessionId(session.sessionId); // Сохраняем UUID сессии в состоянии
+        } catch (error) {
+            console.error("Ошибка при создании сессии", error);
+        }
+    };
+
+    const startGame = async () => {
+        try {
+            const data = await startGameSession(session); // Логика генерации или получения UUID сессии
+            console.log(data)
             // setSessionId(uuid); // Сохраняем UUID сессии в состоянии
         } catch (error) {
             console.error("Ошибка при создании сессии", error);
@@ -27,13 +39,15 @@ function GameSession() {
                 <div>
                     <h2>Сессия запущена</h2>
                     <p>UUID: {sessionId}</p>
-                    <button>Старт</button>
+                    <button onClick={startGame}>Старт</button>
                     <button onClick={handleExit}>Завершить сессию</button>
                 </div>
             ) : (
                 <div>
                     <button onClick={startSession}>Запустить сессию</button>
-                    <button onClick={handleExit}>Выйти</button> {/* Кнопка выхода */}
+                    {/*<button onClick={startGame}>Запуск игры</button>*/}
+                    <button onClick={handleExit}>Выйти</button>
+                    {/* Кнопка выхода */}
                 </div>
             )}
         </div>
